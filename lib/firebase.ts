9c8925d47944cb68d4f7bcf4f264dfeeb54ec22c
alopopau-eng <alopopau@gmail.com>
2026-03-07 10:17:23 +0300

    Initial commit

---
+++ lib/firebase.ts
@@ -0,0 +1,23 @@
+// firebase.ts
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+ apiKey: "AIzaSyAlN1uJ4dUYlePaZ2GPS_CJKlspufkoSDM",
+  authDomain: "jadedozaim.firebaseapp.com",
+  databaseURL: "https://jadedozaim-default-rtdb.firebaseio.com",
+  projectId: "jadedozaim",
+  storageBucket: "jadedozaim.firebasestorage.app",
+  messagingSenderId: "234913494157",
+  appId: "1:234913494157:web:a4d70da6127fc37a189ec4",
+  measurementId: "G-XHVP1LCG26"
+}
+
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const database = getDatabase(app);
+
+export { auth, db, database };
